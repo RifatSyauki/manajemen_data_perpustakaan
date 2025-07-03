54a00944ba6a41dc9fb273e863510998a5d4bfb1
RifatSyauki <rifatlampung658@gmail.com>
2025-07-03 16:51:10 +0700

    FEAT :Add migration, model, seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,16 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Admin;
+use App\Models\Users;
+use App\Models\Book;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+use Illuminate\Support\Facades\Hash;
+
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +20,48 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // Admin::create();
+        $books = [
+            [
+                'title' => 'Kalkulus',
+                'author' => 'Dale Varberg, Purcell, Edwin J, Rigdon, Steven E',
+                'isbn' => '978-979-033-937-8',
+                'Publisher' => 'Erlangga, 2010 ',
+                'publication' => 2010,
+                'Edition' => '9',
+                'shelf' => '500',
+                'availability' => '3'
+            ]
+        ];
+        $users = [
+            [
+                'username' => 'Andi',
+                'email' => 'andi@gmail.com',
+                'password' => Hash::make('andi123')
+            ],
+            [
+                'username' => 'Budi',
+                'email' => 'budi@gmail.com',
+                'password' => Hash::make('budi123')
+            ],
+            [
+                'username' => 'Caca',
+                'email' => 'caca@gmail.com',
+                'password' => Hash::make('caca123')
+            ]
+        ];
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        foreach($users as $user) {
+            Users::create($user);
+        }
+
+        Admin::create([
+            'username' => 'admin',
+            'password' => Hash::make('admin'),
         ]);
+
+        foreach($books as $book){
+            Book::create($book);
+        }
     }
 }
